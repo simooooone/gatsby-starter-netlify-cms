757a42c59947b6c264a5829fdb95b012967a8a66
Simone Foschi <s.foschi@gmail.com>
2019-04-28 14:59:57 +0200

    Indicizzazioni cambiate in home

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'OkGrazie a classifieds website',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'OkGrazie was a classifieds website developed in Visual Basic .NET, C# and SQL Server in 2 years of work, to dempnstrate my ability in the programming field',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
