6d4b07ca0e1e825b6530f8f93e853d4131b9b46d
Simone Foschi <s.foschi@gmail.com>
2019-05-31 21:41:48 +0200

    Updated npm includes, added bootstrap, fixed bugs

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -22,7 +22,7 @@ export const IndexPageTemplate = ({
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-        backgroundPosition: `top left`,
+        backgroundPosition: `top center`,
         backgroundAttachment: `fixed`,
       }}
     >
